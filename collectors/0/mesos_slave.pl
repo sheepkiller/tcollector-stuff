26afd507a1fb51fa2a39d2688d3cfde5e2843e13
Clement Laforet <sheepkiller@cotds.org>
2014-11-06 16:43:43 +0100

    Add monitor statistics to slave

--- collectors/0/mesos_slave.pl
+++ collectors/0/mesos_slave.pl
@@ -5,6 +5,7 @@ use JSON;
 use strict;
 use warnings;
 
+
 my $INTERVAL = 15;
 my $MESOS_SLAVE_HOST="localhost";
 my $MESOS_SLAVE_PORT="5051";
@@ -20,6 +21,18 @@ while (1){
       my $k = $m;
       $m =~ s/\//./g;
       print "mesos.$m $now $scalar_metrics->{$k}\n";
+   }
+
+   $json_metrics=get("http://$MESOS_SLAVE_HOST:$MESOS_SLAVE_PORT/monitor/statistics.json") or die "Can't connect mesos slave";
+   $now = time();
+   $scalar_metrics = JSON->new->utf8->decode($json_metrics);
+   for my $task (@$scalar_metrics) {
+     my $stats = $task->{'statistics'};
+     foreach my $m (keys %{$stats})  {
+        my $k = $m;
+        $m =~ s/\//./g;
+        print "mesos.slave.monitor.stats.$k $now $stats->{$k} task_id=$task->{'executor_id'}\n";
+     }
    }
    sleep $INTERVAL;
 }
