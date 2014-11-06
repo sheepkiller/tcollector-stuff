d6ddfbbf93c2ab7d1cba922d04a21e8b107cbe60
Clement Laforet <sheepkiller@cotds.org>
2014-11-06 21:35:05 +0100

    add cgroup metrics for slave

--- collectors/0/mesos_slave.pl
+++ collectors/0/mesos_slave.pl
@@ -5,10 +5,20 @@ use JSON;
 use strict;
 use warnings;
 
-
 my $INTERVAL = 15;
 my $MESOS_SLAVE_HOST="localhost";
 my $MESOS_SLAVE_PORT="5051";
+
+# 1: read metric names from file
+# 0: filename is metric name
+my $cgroup_files = ();
+$cgroup_files->{'cpuacct'}->{'cpuacct.stat'};
+$cgroup_files->{'cpu'}->{'cpu.stat'}=1;
+$cgroup_files->{'memory'}->{'memory.memsw.failcnt'}=0;
+$cgroup_files->{'memory'}->{'memory.stat'}=1;
+$cgroup_files->{'memory'}->{'memory.usage_in_bytes'}=0;
+$cgroup_files->{'memory'}->{'memory.max_usage_in_bytes'}=0;
+
 
 # flush after every write
 $| = 1;
@@ -22,7 +32,8 @@ while (1){
       $m =~ s/\//./g;
       print "mesos.$m $now $scalar_metrics->{$k}\n";
    }
-
+   my $json_mesos_state = get("http://$MESOS_SLAVE_HOST:$MESOS_SLAVE_PORT/state.json") or die  "Can't connect mesos slave";
+   my $scalar_mesos_state =  JSON->new->utf8->decode($json_mesos_state);
    $json_metrics=get("http://$MESOS_SLAVE_HOST:$MESOS_SLAVE_PORT/monitor/statistics.json") or die "Can't connect mesos slave";
    $now = time();
    $scalar_metrics = JSON->new->utf8->decode($json_metrics);
@@ -31,9 +42,35 @@ while (1){
      foreach my $m (keys %{$stats})  {
         my $k = $m;
         $m =~ s/\//./g;
-        print "mesos.slave.monitor.stats.$k $now $stats->{$k} task_id=$task->{'executor_id'}\n";
+        print "mesos.slave.monitor.stats.$k $now $stats->{$k} task_id=$task->{'executor_id'} framework_id=$task->{'framework_id'} \n";
      }
-   }
+     my @frameworks = @{$scalar_mesos_state->{'frameworks'}};
+     foreach my $f (@frameworks) {
+        next unless $f->{'id'} eq $task->{'framework_id'};
+        my @tasks = @{$f->{'executors'}};
+        foreach my $t (@tasks) {
+             next unless $t->{'id'} eq $task->{'executor_id'};
+             foreach my $k (keys %{$cgroup_files}) {
+                  my $tmp = $cgroup_files->{$k};
+                  foreach my $kk (keys %{$tmp}) {
+                      open(FH, $scalar_mesos_state->{flags}->{cgroups_hierarchy} . "/$k/" . $scalar_mesos_state->{flags}->{cgroups_root}. "/" . $t->{'container'} . "/$kk");
+                      if ($tmp->{$kk} == "0") {
+                          my $v = <FH>;
+                          chomp($v);
+                          my $kkk = $kk =~ s/$k\.//g;
+                          print "mesos.slave.monitor.cgroup.$k.$kk $now $v task_id=$task->{'executor_id'} framework_id=$task->{'framework_id'}\n";
+                      } else {
+                        while(my $line=<FH>) {
+                           chomp($line);
+                           my ($kkk, $v)=split(/\s+/,$line);
+                           print "mesos.slave.monitor.cgroup.$k.$kkk $now $v task_id=$task->{'executor_id'} framework_id=$task->{'framework_id'}\n";
+                        }
+                     }
+                }
+             }
+         }
+       }
+     }
    sleep $INTERVAL;
 }
 
